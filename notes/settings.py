242519adb8dd55f6c4d95e18287cb908d3332ba9
Hemant Vishwakarma <hkvishwakarma01@gmail.com>
2025-12-09 16:14:51 +0530

    database connection change to supabase

--- notes/settings.py
+++ notes/settings.py
@@ -91,13 +91,43 @@ WSGI_APPLICATION = 'notes.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        ssl_require=False
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+#         conn_max_age=600,
+#         ssl_require=False
+#     )
+# }
+
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env
+load_dotenv()
+
+# Fetch variables
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
+
+# Connect to the database
+try:
+    connection = psycopg2.connect(
+        user=USER,
+        password=PASSWORD,
+        host=HOST,
+        port=PORT,
+        dbname=DBNAME
     )
-}
+    print("Connection successful!")
+    
+    
+
+except Exception as e:
+    print(f"Failed to connect: {e}")
 
 
 
